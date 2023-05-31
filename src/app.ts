98e21b2db6357a9ef4c19b08036e408ee0002e49
Nur Mohammad <mohammadnur5480@gmail.com>
2023-05-31 15:15:32 +0600

    [Done] Setup express,mongoose,dotenv,cors,parser

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express()
+
+
+app.use(cors())
+
+// parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req:Request, res:Response, next:NextFunction) => {
+  res.send('Set Up Successfully')
+})
+
+export default app;
