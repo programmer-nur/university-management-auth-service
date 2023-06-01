59352ea9c9d0f8e36127e2c58239c1c607ae3a50
Nur Mohammad <mohammadnur5480@gmail.com>
2023-06-01 21:25:04 +0600

    [Done] Setup Lint-staged , prettier

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app: Application = express()
-
 
 app.use(cors())
 
 // parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req:Request, res:Response, next:NextFunction) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Set Up Successfully')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('Database is connected Successfully')
-        app.listen(config.prot, () => {
-            console.log(`Application is listening on port ${config.prot}`)
-          })
-    } catch (error) {
-        console.log('Failed to connect database',error)
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is connected Successfully')
+    app.listen(config.prot, () => {
+      console.log(`Application is listening on port ${config.prot}`)
+    })
+  } catch (error) {
+    console.log('Failed to connect database', error)
+  }
 }
 
 main()
