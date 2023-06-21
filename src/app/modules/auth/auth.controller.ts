01857719e2b08e079fddcd8f7b0dc5e2367cabea
Nur Mohammad <mohammadnur5480@gmail.com>
2023-06-21 20:10:58 +0600

    [Done] set refreshcookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,15 +4,23 @@ import sendResponse from '../../../shared/sendRespons';
 import httpStatus from 'http-status';
 import { AuthService } from './auth.services';
 import { ILoginResponse } from './auth.interface';
+import config from '../../../config';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
   const result = await AuthService.loginUser(loginData);
+  // set cookies
+  const { refreshToken, ...others } = result;
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
   sendResponse<ILoginResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User login  successfully',
-    data: result,
+    data: others,
   });
 });
 
